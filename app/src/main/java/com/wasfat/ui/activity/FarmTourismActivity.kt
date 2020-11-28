50bc74591df70977818cce35188ea7a275dcc5aa
Tushar Dave <imtushardave@gmail.com>
2020-11-28 22:08:59 +0530

    Farm form updated

--- app/src/main/java/com/wasfat/ui/activity/FarmTourismActivity.kt
+++ app/src/main/java/com/wasfat/ui/activity/FarmTourismActivity.kt
@@ -2,14 +2,18 @@ package com.wasfat.ui.activity
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -26,8 +30,10 @@ import com.wasfat.network.RestApiFactory
 import com.wasfat.ui.base.BaseBindingActivity
 import com.wasfat.ui.pojo.*
 import com.wasfat.ui.pojo.Unit
+import com.wasfat.utils.Constants
 import com.wasfat.utils.ProgressDialog
 import com.wasfat.utils.UtilityMethod
+import kotlinx.android.synthetic.main.activity_register.*
 import pl.aprilapps.easyphotopicker.DefaultCallback
 import pl.aprilapps.easyphotopicker.EasyImage
 import retrofit2.Call
@@ -37,8 +43,22 @@ import java.io.File
 
 class FarmTourismActivity : BaseBindingActivity() {
 
+    private var cityId: Int = -1
+    private var stateId: Int = -1
+    private var blockId: Int = -1
+
     var binding: ActivityFarmTourismBinding? = null
     var onClickListener: View.OnClickListener? = null
+    var stateList: ArrayList<Statelist> = ArrayList()
+    var stateNameList: ArrayList<String> = ArrayList()
+    var cityList: ArrayList<Citylist> = ArrayList()
+    var cityNameList: ArrayList<String> = ArrayList()
+    var blockList: ArrayList<Blocklist> = ArrayList()
+    var blockNameList: ArrayList<String> = ArrayList()
+
+    val reqDataState: HashMap<String, Int> = HashMap()
+    val reqDataCity: HashMap<String, Int> = HashMap()
+    val reqDataBlock: HashMap<String, Int> = HashMap()
 
     var selectedImageFilePath : String? = null
 
@@ -63,19 +83,56 @@ class FarmTourismActivity : BaseBindingActivity() {
 
     override fun initializeObject() {
         onClickListener = this
+
+        callGetStateListByCountryAPI()
+
     }
 
     override fun setListeners() {
+
         binding!!.imvClose.setOnClickListener(onClickListener)
         binding!!.rlImage.setOnClickListener (onClickListener)
         binding!!.imvRemoveImage.setOnClickListener(onClickListener)
         binding!!.btnAddFarmTourism.setOnClickListener(onClickListener)
+        binding!!.btnAddressMap.setOnClickListener(onClickListener)
+        binding!!.edtState.setOnClickListener(onClickListener)
+        binding!!.edtCity.setOnClickListener(onClickListener)
+        binding!!.edtBlock.setOnClickListener(onClickListener)
+
     }
 
     override fun onClick(view: View?) {
         when (view!!.id) {
             R.id.imvClose -> {
                 finish()
+            }
+            R.id.edtState -> {
+                selectDialog(
+                    getString(R.string.select_state),
+                    stateNameList,
+                    binding!!.edtState,
+                    reqDataState
+                )
+            }
+            R.id.edtCity -> {
+                selectDialog(
+                    getString(R.string.select_City),
+                    cityNameList,
+                    binding!!.edtCity,
+                    reqDataCity
+                )
+            }
+            R.id.edtBlock -> {
+                selectDialog(
+                    getString(R.string.select_block),
+                    blockNameList,
+                    binding!!.edtBlock,
+                    reqDataBlock
+                )
+            }
+            R.id.btnAddressMap ->{
+                //TODO: Open MAP for address
+                Toast.makeText(mActivity!!,"Map Will Open Here",Toast.LENGTH_SHORT).show()
             }
             R.id.imvRemoveImage -> {
                 selectedImageFilePath = null
@@ -93,20 +150,15 @@ class FarmTourismActivity : BaseBindingActivity() {
 
                 Log.d("TAG", "onClick: BTN CLICKED")
 
-                Log.d("TAG", "onClick: " + isValidFormData(
-                    binding!!.edtFarmName.text.toString(),
-                    binding!!.edtAddress.text.toString(),
-                    binding!!.edtContactNumber.text.toString(),
-                    binding!!.edtFacilities.text.toString(),
-                    binding!!.edtAtraction.text.toString()
-                ))
-
                 if (isValidFormData(
                         binding!!.edtFarmName.text.toString(),
                         binding!!.edtAddress.text.toString(),
                         binding!!.edtContactNumber.text.toString(),
                         binding!!.edtFacilities.text.toString(),
-                        binding!!.edtAtraction.text.toString()
+                        binding!!.edtAtraction.text.toString(),
+                        binding!!.edtEmail.text.toString(),
+                        binding!!.edtWebsite.text.toString(),
+                        binding!!.edtPrice.text.toString()
                     )
                 ) {
 
@@ -118,12 +170,178 @@ class FarmTourismActivity : BaseBindingActivity() {
                             binding!!.edtFacilities.text.toString(),
                             binding!!.edtAtraction.text.toString(),
                             binding!!.cbPublished.isChecked,
-                            it
+                            it,
+                            binding!!.edtEmail.text.toString(),
+                            binding!!.edtWebsite.text.toString(),
+                            binding!!.edtPrice.text.toString()
                         )
                     }
                 }
             }
         }
+    }
+
+    private fun selectDialog(
+        title: String,
+        stateList: List<String>,
+        edtState: EditText,
+        reqData: HashMap<String, Int>
+    ) {
+        val view1: View =
+            (getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(
+                R.layout.list_search_item,
+                null,
+                false
+            )
+        val dialog = android.app.AlertDialog.Builder(this@FarmTourismActivity)
+            .setTitle(title)
+            .setView(view1).create()
+        val lv = view1.findViewById<View>(R.id.listView) as ListView
+        val edtSearch = view1.findViewById<View>(R.id.edtSearch) as EditText
+        val arrayAdapter = ArrayAdapter(
+            this@FarmTourismActivity,
+            android.R.layout.simple_list_item_1,
+            stateList
+        )
+        lv.adapter = arrayAdapter
+        lv.onItemClickListener =
+            AdapterView.OnItemClickListener { parent: AdapterView<*>, view: View?, position: Int, id: Long ->
+                val entry = parent.adapter.getItem(position) as String
+                if (title == getString(R.string.select_state)) {
+                    edtState.setText(entry)
+                    edtCity.setText("")
+                    edtBlock.setText("")
+                    stateId = reqData[entry]!!
+                    callGetCityListByStateAPI()
+                } else if (title == getString(R.string.select_City)) {
+                    edtCity.setText(entry)
+                    edtBlock.setText("")
+                    cityId = reqData[entry]!!
+                    callGetBlockListByStateAPI()
+                } else {
+                    edtBlock.setText(entry)
+                    blockId = reqData[entry]!!
+                }
+                dialog.cancel()
+            }
+        dialog.show()
+        edtSearch.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                arrayAdapter.filter.filter(s.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+    }
+
+
+    private fun callGetStateListByCountryAPI() {
+        ProgressDialog.showProgressDialog(mActivity!!)
+        var gsonObject = JsonObject()
+        val rootObject = JsonObject()
+        rootObject.addProperty("languageId", "1")
+        rootObject.addProperty("countryId", Constants.COUNTRYID)
+        var jsonParser = JsonParser()
+        gsonObject = jsonParser.parse(rootObject.toString()) as JsonObject
+        val apiService1 = RestApiFactory.getAddressClient()!!.create(RestApi::class.java)
+        val call1: Call<StateResponsePOJO> = apiService1.getStateListByCountryId(gsonObject)
+        call1.enqueue(object : Callback<StateResponsePOJO?> {
+            override fun onResponse(
+                call: Call<StateResponsePOJO?>,
+                response: Response<StateResponsePOJO?>
+            ) {
+                ProgressDialog.hideProgressDialog()
+                if (response.body() != null) {
+                    if (response.isSuccessful) {
+                        stateList = response.body()!!.statelist
+                        for (state in stateList) {
+                            stateNameList.add(state.Name)
+                            reqDataState[state.Name] = state.Id
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<StateResponsePOJO?>, t: Throwable) {
+                ProgressDialog.hideProgressDialog()
+            }
+        })
+
+
+    }
+
+    private fun callGetCityListByStateAPI() {
+        ProgressDialog.showProgressDialog(mActivity!!)
+        var gsonObject = JsonObject()
+        val rootObject = JsonObject()
+        rootObject.addProperty("languageId", "1")
+        rootObject.addProperty("stateId", stateId)
+        var jsonParser = JsonParser()
+        gsonObject = jsonParser.parse(rootObject.toString()) as JsonObject
+        val apiService1 = RestApiFactory.getAddressClient()!!.create(RestApi::class.java)
+        val call1: Call<CityResponsePOJO> = apiService1.getCityListByStateId(gsonObject)
+        call1.enqueue(object : Callback<CityResponsePOJO?> {
+            override fun onResponse(
+                call: Call<CityResponsePOJO?>,
+                response: Response<CityResponsePOJO?>
+            ) {
+                ProgressDialog.hideProgressDialog()
+                if (response.body() != null) {
+                    if (response.isSuccessful) {
+                        cityList = response.body()!!.citylist
+                        for (city in cityList) {
+                            cityNameList.add(city.Name)
+                            reqDataCity[city.Name] = city.Id
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<CityResponsePOJO?>, t: Throwable) {
+                ProgressDialog.hideProgressDialog()
+            }
+        })
+
+
+    }
+
+    private fun callGetBlockListByStateAPI() {
+        ProgressDialog.showProgressDialog(mActivity!!)
+        var gsonObject = JsonObject()
+        val rootObject = JsonObject()
+        rootObject.addProperty("languageId", "1")
+        rootObject.addProperty("cityId", cityId)
+        var jsonParser = JsonParser()
+        gsonObject = jsonParser.parse(rootObject.toString()) as JsonObject
+        val apiService1 = RestApiFactory.getAddressClient()!!.create(RestApi::class.java)
+        val call1: Call<BlockResponsePOJO> = apiService1.getBlockListByStateId(gsonObject)
+        call1.enqueue(object : Callback<BlockResponsePOJO?> {
+            override fun onResponse(
+                call: Call<BlockResponsePOJO?>,
+                response: Response<BlockResponsePOJO?>
+            ) {
+                ProgressDialog.hideProgressDialog()
+                if (response.body() != null) {
+                    if (response.isSuccessful) {
+                        blockList = response.body()!!.blocklist
+                        for (block in blockList) {
+                            blockNameList.add(block.Name)
+                            reqDataBlock[block.Name] = block.Id
+                        }
+                    }
+                }
+            }
+            override fun onFailure(call: Call<BlockResponsePOJO?>, t: Throwable) {
+
+                Log.d("RegisterActivity", "onFailure: " + t.localizedMessage)
+
+                ProgressDialog.hideProgressDialog()
+            }
+        })
     }
 
     private fun addFarmTourismItem(
@@ -133,7 +351,10 @@ class FarmTourismActivity : BaseBindingActivity() {
         facilities: String,
         attractions: String,
         published : Boolean,
-        imageBase64 : String
+        imageBase64 : String,
+        email : String,
+        website : String,
+        price : String
     ) {
 
 
@@ -150,6 +371,10 @@ class FarmTourismActivity : BaseBindingActivity() {
         rootObject.addProperty("lmg", 0)
         rootObject.addProperty("UserId", sessionManager!!.userId)
         rootObject.addProperty("Published", published)
+        rootObject.addProperty("BlockID", blockId)
+        rootObject.addProperty("EmailId", email)
+        rootObject.addProperty("Website", website)
+        rootObject.addProperty("Price", price)
         rootObject.addProperty("Image", imageBase64)
 
 
@@ -193,7 +418,10 @@ class FarmTourismActivity : BaseBindingActivity() {
         address: String,
         contactNumber: String,
         facilities: String,
-        attractions: String
+        attractions: String,
+        email : String,
+        website : String,
+        price : String
     ): Boolean {
 
         Log.d("TAG", "isValidFormData: Validation is working" )
@@ -220,6 +448,21 @@ class FarmTourismActivity : BaseBindingActivity() {
 
         if (TextUtils.isEmpty(attractions)) {
             UtilityMethod.showToastMessageError(mActivity!!,getString(R.string.enter_farm_attraction))
+            return false
+        }
+
+        if (TextUtils.isEmpty(email)) {
+            UtilityMethod.showToastMessageError(mActivity!!,getString(R.string.enter_farm_contact_email))
+            return false
+        }
+
+        if (TextUtils.isEmpty(website)) {
+            UtilityMethod.showToastMessageError(mActivity!!,getString(R.string.enter_farm_website))
+            return false
+        }
+
+        if (TextUtils.isEmpty(price)) {
+            UtilityMethod.showToastMessageError(mActivity!!,getString(R.string.enter_farm_price))
             return false
         }
 
